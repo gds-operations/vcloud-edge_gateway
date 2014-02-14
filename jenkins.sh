e03283daf9382a81a43382d33db21647d80f55f4
Anna Shipman <anna@annashipman.co.uk>
2014-02-14 16:08:47 +0000

    [#65737418] Make jenkins call integration tests
    Once gem_publisher is included, a successful run of this job will deploy to rubygems, so it should run the integration tests against one environment at least.

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -x
 set -e
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
-bundle exec rake 
+bundle exec rake
+
+./scripts/generate_fog_conf_file.sh
+export FOG_RC=fog_integration_test.config
+bundle exec rake integration
+rm fog_integration_test.config
