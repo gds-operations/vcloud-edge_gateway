4119af69a0a1063b2d1bf76f86c8ac6e6275ebf0
Anna Shipman <anna@annashipman.co.uk>
2014-03-14 19:16:08 +0000

    Artefacts of the way inheritance was being done

--- lib/vcloud/edge_gateway/configuration_differ.rb
+++ lib/vcloud/edge_gateway/configuration_differ.rb
@@ -5,8 +5,6 @@ module Vcloud
         def initialize local, remote
           @local = local
           @remote = remote
-          @stripped_local = nil
-          @stripped_remote = nil
         end
 
         def diff
@@ -14,14 +12,12 @@ module Vcloud
         end
 
         def stripped_local_config
-          return @stripped_local unless @stripped_local.nil?
           return nil if @local.nil?
           @stripped_local = strip_unused_field_from_config(@local)
           @stripped_local
         end
 
         def stripped_remote_config
-          return @stripped_remote unless @stripped_remote.nil?
           return nil if @remote.nil?
           @stripped_remote = strip_unused_field_from_config(@remote)
           @stripped_remote
