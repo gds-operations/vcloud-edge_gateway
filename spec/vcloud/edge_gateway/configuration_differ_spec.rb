9883ea53ef28b8fe80181b0278a5df733841647e
Anna Shipman <anna@annashipman.co.uk>
2014-03-14 20:17:38 +0000

    Looped through tests are bad
    Instead of failing, they just don't run if the circumstances aren't right. For example:
     - 0f29d677eca: one of tests wasn't running due to a very small typo
     - https://github.com/alphagov/vcloud-core/commit/ebd330587: the code they were testing is no longer present, so the tests just don't run; when I remove code that is tested, I expect the tests to fail.
    
    This makes them fairly useless as tests. This commit reimplements the tests as actual tests. They only test the configuration differ file as this stage.

--- spec/vcloud/edge_gateway/configuration_differ_spec.rb
+++ spec/vcloud/edge_gateway/configuration_differ_spec.rb
@@ -1,15 +1,129 @@
 require 'spec_helper'
-require_relative './common_differ_test_cases'
 
 module Vcloud
   module EdgeGateway
     describe ConfigurationDiffer do
 
-      COMMON_DIFFER_TEST_CASES.each do |test_case|
-        it "#{test_case[:title]}" do
-          differ = ConfigurationDiffer.new(test_case[:src], test_case[:dest])
-          expect(differ.diff).to eq(test_case[:output])
-        end
+      it 'should return an empty array for two identical empty Hashes' do
+        local = { }
+        remote = { }
+        output = []
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should return an empty array for two identical simple Hashes' do
+        local = { testing: 'testing', one: 1, two: 'two', three: "3" }
+        remote = { testing: 'testing', one: 1, two: 'two', three: "3" }
+        output =  []
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should return an empty array for two identical deep Hashes' do
+        local = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        remote = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        output = []
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should highlight a simple addition' do
+        local = { foo: '1' }
+        remote = { foo: '1', bar: '2' }
+        output = [["+", "bar", "2"]]
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should highlight a simple subtraction' do
+        local = { foo: '1', bar: '2' }
+        remote = { foo: '1' }
+        output = [["-", "bar", "2"]]
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should highlight a deep addition' do
+        local = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        remote = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5, 6 ] },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        output = [["+", "deep[0].deeper[5]", 6]]
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should highlight a deep subtraction' do
+        local = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        remote = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 6, 5, 3, 2 ] },
+        ]}
+        output =  [["-", "deep[1].deeper[2]", 4]]
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should return an empty array when hash params are reordered' do
+        local = { one: 1, testing: 'testing', deep: [
+          { deeper: [ 1, 2, 3, 4, 5 ], foo: 'bar' },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        remote = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        output = []
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should highlight when array elements are reordered' do
+        local = { testing: 'testing', one: 1, deep: [
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+        ]}
+        remote = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        output = [
+          ["+", "deep[0]", {:foo=>"bar", :deeper=>[1, 2, 3, 4, 5]}],
+          ["-", "deep[2]", {:foo=>"bar", :deeper=>[1, 2, 3, 4, 5]}],
+        ]
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
+      end
+
+      it 'should highlight when deep array elements are reordered' do
+        local = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 5, 6, 4, 3, 2 ] },
+        ]}
+        remote = { testing: 'testing', one: 1, deep: [
+          { foo: 'bar', deeper: [ 1, 2, 3, 4, 5 ] },
+          { baz: 'bop', deeper: [ 6, 5, 4, 3, 2 ] },
+        ]}
+        output =  [
+          ["+", "deep[1].deeper[0]", 6],
+          ["-", "deep[1].deeper[2]", 6]
+        ]
+        differ = ConfigurationDiffer.new(local, remote)
+        expect(differ.diff).to eq(output)
       end
 
     end
