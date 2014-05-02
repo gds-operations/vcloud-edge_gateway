c65ddcf12ea53c3be02fe187a4a94e6f145f5a7b
Dan Carley <dan.carley@gmail.com>
2014-05-02 15:13:31 +0100

    Helper methods for locating integration fixtures
    Because I'm about to refactor `#generate_input_config_file` within each of
    the integration tests which currently does `File.expand_path("data/…`.

--- spec/support/integration_helper.rb
+++ spec/support/integration_helper.rb
@@ -18,4 +18,12 @@ module IntegrationHelper
     end
     Kernel.exit(2) if error
   end
+
+  def self.fixture_path
+    File.expand_path("../integration/edge_gateway/data", File.dirname(__FILE__))
+  end
+
+  def self.fixture_file(path)
+    File.join(self.fixture_path, path)
+  end
 end
