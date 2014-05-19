fdf04e9d7940d7dfe15590a1455597316d6b6869
Matt Bostock <matt@mattbostock.com>
2014-05-19 12:05:51 +0100

    Refactor update_required?
    Refactor
    `Vcloud::EdgeGateway::EdgeGatewayConfiguration.update_required?` so that
    it does not modify
    `Vcloud::EdgeGateway::EdgeGatewayConfiguration.config`, which is
    potentially confusiong for a method that by convention should only
    return a boolean.
    
    The logic in `update_required?` has moved to the `config()` method,
    which has been renamed to `generate_new_config()` to make the name more
    descriptive.
    
    I have also removed `local_config`, `remote_config`, and
    `edge_gateway_interfaces` as instance variables; these are now passed to
    `generate_new_config()`.
    
    `generate_new_config()` is now a private method so that we can change our
    internal interfaces more easily without worrying about making
    what would have been breaking changes; it's also called immediately from
    the `initialize()` method.
    
    `generate_new_config()` does not update the `@config` instance variable,
    rather it updates a `new_config` method variable which it then returns.
    I've added an `attr_reader` for the `@config` instance variable
    accordingly.

--- lib/vcloud/edge_gateway/edge_gateway_configuration.rb
+++ lib/vcloud/edge_gateway/edge_gateway_configuration.rb
@@ -2,63 +2,57 @@ module Vcloud
   module EdgeGateway
     class EdgeGatewayConfiguration
 
+      attr_reader :config
+
       def initialize(local_config, remote_config, edge_gateway_interfaces)
-        @local_config = local_config
-        @remote_config = remote_config
-        @edge_gateway_interfaces = edge_gateway_interfaces
-        @config = { }
-        @update_required = nil
+        @config = generate_new_config(local_config, remote_config, edge_gateway_interfaces)
       end
 
       def update_required?
-        @update_required = false
+        @config.any?
+      end
 
-        firewall_service_config = EdgeGateway::ConfigurationGenerator::FirewallService.new.generate_fog_config(@local_config[:firewall_service])
+      private
+      def generate_new_config(local_config, remote_config, edge_gateway_interfaces)
+        new_config = { }
+
+        firewall_service_config = EdgeGateway::ConfigurationGenerator::FirewallService.new.generate_fog_config(local_config[:firewall_service])
+
         unless firewall_service_config.nil?
-          differ = EdgeGateway::FirewallConfigurationDiffer.new(firewall_service_config, @remote_config[:FirewallService])
+          differ = EdgeGateway::FirewallConfigurationDiffer.new(firewall_service_config, remote_config[:FirewallService])
           unless differ.diff.empty?
-            @config[:FirewallService] = firewall_service_config
-            @update_required = true
+            new_config[:FirewallService] = firewall_service_config
           end
         end
 
         nat_service_config = EdgeGateway::ConfigurationGenerator::NatService.new(
-          @local_config[:nat_service],
-          @edge_gateway_interfaces
+          local_config[:nat_service],
+          edge_gateway_interfaces
         ).generate_fog_config
 
         unless nat_service_config.nil?
-          differ = EdgeGateway::NatConfigurationDiffer.new(nat_service_config, @remote_config[:NatService])
+          differ = EdgeGateway::NatConfigurationDiffer.new(nat_service_config, remote_config[:NatService])
           unless differ.diff.empty?
-            @config[:NatService] = nat_service_config
-            @update_required = true
+            new_config[:NatService] = nat_service_config
           end
         end
 
         load_balancer_service_config =
           EdgeGateway::ConfigurationGenerator::LoadBalancerService.new(
-            @edge_gateway_interfaces
-          ).generate_fog_config(@local_config[:load_balancer_service])
+            edge_gateway_interfaces
+          ).generate_fog_config(local_config[:load_balancer_service])
 
         unless load_balancer_service_config.nil?
           differ = EdgeGateway::LoadBalancerConfigurationDiffer.new(
                      load_balancer_service_config,
-                     @remote_config[:LoadBalancerService]
+                     remote_config[:LoadBalancerService]
                    )
           unless differ.diff.empty?
-            @config[:LoadBalancerService] = load_balancer_service_config
-            @update_required = true
+            new_config[:LoadBalancerService] = load_balancer_service_config
           end
         end
 
-        @update_required
-      end
-
-      def config
-        if @update_required.nil?
-          update_required?
-        end
-        @config
+        new_config
       end
 
     end
