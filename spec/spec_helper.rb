f1920ebad206bf0de951d102f6955a0f83bc91d6
Matt Bostock <matt@mattbostock.com>
2014-06-11 17:22:30 +0100

    Change SimpleCov behaviour to stop reporting
    Change the SimpleCov behaviour so that it no longer reports coverage
    when `rake` completes and acts only as a 'red light' if coverage drops
    significantly (by 20%) or drops below a percentage (80%).
    
    Note that to silence the 'Coverage report generated for' message that
    reports the coverage percentage when SimpleCov completes, we
    monkey-patch the SimpleCov::Formatter::HTMLFormatter class.
    
    Also, `SimpleCov#result.format!` is called by default when SimpleCov
    exits[1], so there's no need for us to explicitly call it.
    
    The SimpleCov::Formatter::HTMLFormatter#output_message _only_ prints the
    'Coverage report generated for' message, so by monkey-patching it we
    don't lose the other warnings that we want to keep (mentioned above).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
-require 'support/integration_helper'
-
+# SimpleCov must run _first_ according to its README
 if ENV['COVERAGE']
   require 'simplecov'
+
+  # monkey-patch to prevent SimpleCov from reporting coverage percentage
+  class SimpleCov::Formatter::HTMLFormatter
+    def output_message(_message)
+      nil
+    end
+  end
 
   SimpleCov.profiles.define 'gem' do
     add_filter '/spec/'
@@ -11,9 +17,12 @@ if ENV['COVERAGE']
     add_group 'Libraries', '/lib/'
   end
 
+  SimpleCov.minimum_coverage(80)
+  SimpleCov.maximum_coverage_drop(20)
   SimpleCov.start 'gem'
 end
 
+require 'support/integration_helper'
 require 'bundler/setup'
 require 'vcloud/edge_gateway'
 require 'vcloud/tools/tester'
@@ -21,17 +30,5 @@ require 'vcloud/tools/tester'
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
-  end
-end
-
-if ENV['COVERAGE']
-  SimpleCov.at_exit do
-    SimpleCov.result.format!
-    # do not change the coverage percentage, instead add more unit tests to fix coverage failures.
-    if SimpleCov.result.covered_percent < 90
-      print "ERROR::BAD_COVERAGE\n"
-      print "Coverage is less than acceptable limit(90%). Please add more tests to improve the coverage\n"
-      exit(1)
-    end
   end
 end
